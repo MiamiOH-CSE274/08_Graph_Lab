e514357b971c5730cb9cb0a8eedb0f3355ce8a34
ethan2014 <richard_stroker@aim.com>
2014-11-13 11:38:48 -0500

    fixed indentation and formatting

--- Graph.cpp
+++ Graph.cpp
@@ -5,23 +5,27 @@
  */
 #include "Graph.h"
 
-Graph::Graph(unsigned int numNodes){
-  //TODO
+Graph::Graph(unsigned int numNodes)
+{
+    //TODO
 }
 
-int Graph::getCost(int node1, int node2){
-  //TODO
-  return -1;
+int Graph::getCost(int node1, int node2)
+{
+    //TODO
+    return -1;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
-void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+void Graph::addEdge(int node1, int node2, double cost)
+{
+    //TODO
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
-void Graph::removeEdge(int node1, int node2){
-  //TODO
+void Graph::removeEdge(int node1, int node2)
+{
+    //TODO
 }
