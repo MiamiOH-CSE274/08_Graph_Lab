1bb82a72480385afc13ea46297b3b31d2da90f0e
Samsab <bowdlesa@miamioh.edu>
2014-11-13 12:12:47 -0500

    Finished, I think. Seems too easy.

--- Graph.cpp
+++ Graph.cpp
@@ -1,27 +1,46 @@
 /*****
- * Author   : brinkmwj
- * Date     : 2009-11-06
- * Sources  : All code is original
- */
+* Author   : Sam Bowdler
+* Date     : 2014-11-13
+* Sources  : All code is original, template methods given from brinkmwj
+*/
 #include "Graph.h"
 
-Graph::Graph(unsigned int numNodes){
-  //TODO
+Graph::Graph(unsigned int numNodes) {
+	adjList.resize(numNodes);
 }
 
-int Graph::getCost(int node1, int node2){
-  //TODO
-  return -1;
+double Graph::getCost(int node1, int node2) {
+	for (int i = 0; i < adjList[node1].edgeList.size(); i++)
+		if (adjList[node1].edgeList[i].dest == node2)
+			return adjList[node1].edgeList[i].cost;
+	return -1.0;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
-void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+void Graph::addEdge(int node1, int node2, double cost) {
+	if (cost < 0)
+		throw std::string("ERROR: edge cost < 0");
+
+	adjList[node1].edgeList.push_back(Edge(cost, node2));
+	adjList[node2].edgeList.push_back(Edge(cost, node1));
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
-void Graph::removeEdge(int node1, int node2){
-  //TODO
+void Graph::removeEdge(int node1, int node2) {
+	if(getCost(node1, node2) != -1.0) {
+		for (int i = 0; i < adjList[node1].edgeList.size(); i++) {
+			if (adjList[node1].edgeList[i].dest == node2) {
+				adjList[node1].edgeList.erase(adjList[node1].edgeList.begin() + i);
+				break;
+			}
+		}
+		for (int i = 0; i < adjList[node2].edgeList.size(); i++) {
+			if (adjList[node2].edgeList[i].dest == node1) {
+				adjList[node2].edgeList.erase(adjList[node2].edgeList.begin() + i);
+				return;
+			}
+		}
+	}
 }
