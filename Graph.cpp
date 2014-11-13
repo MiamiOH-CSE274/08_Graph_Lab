927741c9c182065b8783c4609c1dbb7e0818fb02
carolinedanzi <danzicr@miamioh.edu>
2014-11-12 22:28:53 -0500

    First draft of removeEdge

--- Graph.cpp
+++ Graph.cpp
@@ -13,7 +13,7 @@ int Graph::getCost(int node1, int node2){
 	// Look through node1's list of edges to find one with
 	// node2 as its destination, then return that cost
 	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
-		if (adjList[node1].edgeList[i].dest = node2)
+		if (adjList[node1].edgeList[i].dest == node2)
 			return adjList[node1].edgeList[i].cost;
 	}
 	// If there is no edge, return -1
@@ -36,5 +36,12 @@ void Graph::addEdge(int node1, int node2, double cost){
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
+		if (adjList[node1].edgeList[i].dest == node2)
+			adjList[node1].edgeList.erase(adjList[node1].edgeList.begin() + i);
+	}
+	for (unsigned int i = 0; i < adjList[node2].edgeList.size(); i++){
+		if (adjList[node2].edgeList[i].dest == node1)
+			adjList[node2].edgeList.erase(adjList[node2].edgeList.begin() + i);
+	}
 }
