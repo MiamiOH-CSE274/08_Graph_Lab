38e757e5a3bb6f6fc373fa39b4e5b62506df00a1
Chris Dieter <chrisdieter616@yahoo.com>
2014-11-16 18:32:37 -0500

    complete add function and get cost function

--- Graph.cpp
+++ Graph.cpp
@@ -10,13 +10,23 @@ Graph::Graph(unsigned int numNodes){
 }
 
 int Graph::getCost(int node1, int node2){
-  //TODO
-  return -1;
+	int retCost = -1;
+	int edgeListSize = adjList.at(node1).edgeList.size();
+	for(int i = 0; i < edgeListSize; i++){
+		Edge edgeElement = adjList.at(node1).edgeList.at(i);
+		if(edgeElement.dest == node2)
+			retCost = edgeElement.cost;
+	}
+	return retCost;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
+	
+	if(cost < 0)
+		throw std::string("Cost cannot be less than zero");
+	
 	Edge* j = new Edge(cost, node2);
 	Edge* k = new Edge(cost, node1);
 	adjList.at(node1).edgeList.push_back(*(j));
