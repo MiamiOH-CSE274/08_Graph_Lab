764f3532712ec5d39ed371b15fa186aef0963dfd
clarkdb <clarkdb@miamioh.edu>
2014-11-17 09:44:20 -0500

    Added an addEdge method.

--- Graph.cpp
+++ Graph.cpp
@@ -21,11 +21,23 @@ double Graph::getCost(int node1, int node2){
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-	
+	if (cost < 0){
+		throw std::string("Negative cost is not allowed");
+	}
+	for (int x = 0; x < adjList[node1].edgeList.size; x++){
+		if (adjList[node1].edgeList[x].dest == node2){
+			adjList[node1].edgeList[x] = Edge(cost, node2);
+			for (int x = 0; x < adjList[node2].edgeList.size; x++){
+				if (adjList[node2].edgeList[x].dest == node1){
+					adjList[node2].edgeList[x] = Edge(cost, node1);
+				}
+			}
+		}
+	}
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+  
 }
