15aadf3ee28119fdff9959a18527bb0deef438b1
clarkdb <clarkdb@miamioh.edu>
2014-11-17 09:34:48 -0500

    Added a getCost method.

--- Graph.cpp
+++ Graph.cpp
@@ -10,14 +10,18 @@ Graph::Graph(unsigned int numNodes){
 }
 
 double Graph::getCost(int node1, int node2){
-  //TODO
+	for (int x = 0; x < adjList[node1].edgeList.size; x++){
+		if (adjList[node1].edgeList[x].dest == node2){
+			return adjList[node1].edgeList[x].cost;
+		}
+	}
   return -1.0;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+	
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
