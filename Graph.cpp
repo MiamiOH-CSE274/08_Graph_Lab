8c52e137a5af4a7936d4634713046abca89727eb
SiegerJman <reedjs3@miamioh.edu>
2014-11-18 17:43:16 -0500

    finished graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -6,22 +6,59 @@
 #include "Graph.h"
 
 Graph::Graph(unsigned int numNodes){
-  //TODO
+	adjList.resize(numNodes);
+	for (unsigned int i = 0; i < numNodes; i++)
+		adjList[i].edgeList.resize(numNodes - 1);
 }
 
 double Graph::getCost(int node1, int node2){
-  //TODO
-  return -1.0;
+	for (unsigned int i = 0;i<adjList[node1].edgeList.size();i++){
+		if (adjList[node1].edgeList[i].dest == node2){
+			return adjList[node1].edgeList[i].cost;
+		}
+	}
+	return -1;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-  //TODO
+	if (cost < 0){
+		throw std::string("Can't have a negative cost.");
+	}
+	//finds pair node1,node2
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
+		if (adjList[node1].edgeList[i].dest == node2){
+			adjList[node1].edgeList[i].cost = cost;
+			//finds pair node2, node1
+			for (unsigned int j = 0; j < adjList[node2].edgeList.size(); j++){
+				if (adjList[node2].edgeList[j].dest == node1){
+					adjList[node2].edgeList[j].cost = cost;
+					//break to save on processing power
+					break;
+				}
+			}
+		}
+	}
+	adjList[node1].edgeList.push_back(Edge(cost, node2));
+	adjList[node2].edgeList.push_back(Edge(cost, node1));
 }
-
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
+		if (adjList[node1].edgeList[i].dest == node2){
+			adjList[node1].edgeList[i].cost = -1;
+			//finds pair node2, node1
+			for (unsigned int j = 0; j < adjList[node2].edgeList.size(); j++){
+				if (adjList[node2].edgeList[j].dest == node1){
+					adjList[node2].edgeList[j].cost = -1;
+					//break to save on processing power
+					break;
+				}
+			}
+		}
+	}
+	adjList[node1].edgeList.push_back(Edge(-1, node2));
+	adjList[node2].edgeList.push_back(Edge(-1, node1));
 }
