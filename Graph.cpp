0b909b7aa2d23137798b6efb85f008aa966b3971
MabeCr <MabeCr@miamioh.edu>
2014-11-18 23:36:03 -0500

    Getting -1 where expected, but throws an error...

--- Graph.cpp
+++ Graph.cpp
@@ -23,16 +23,29 @@ double Graph::getCost(int node1, int node2){
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
 	if (cost < 0){
-		throw std::string("Can't have a cost less than 0");
+		throw std::string("Error: The cost cannot be lower than 0");
 	}
 
-	Edge one = Edge(cost, node2);
-	Edge two = Edge(cost, node1);
+	
+
+	adjList[node1].edgeList.push_back(Edge(cost, node2));
+	adjList[node2].edgeList.push_back(Edge(cost, node1));
 
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-  //TODO
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
+		if (adjList[node1].edgeList[i].dest == node2) {
+			adjList[node1].edgeList.erase(adjList[node1].edgeList.begin() + i);
+		}
+	}
+
+	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
+		if (adjList[node2].edgeList[i].dest == node1) {
+			adjList[node2].edgeList.erase(adjList[node2].edgeList.begin() + i);
+		}
+	}
+
 }
