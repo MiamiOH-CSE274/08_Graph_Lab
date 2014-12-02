72811f53541a529e7f96d30b4ec6f0f2f70c5ccb
unknown <mccarts3@EAS-BEN006-B4.it.muohio.edu>
2014-12-02 12:30:54 -0500

    Added a traversal to the header file.

--- Graph.h
+++ Graph.h
@@ -43,6 +43,8 @@ public:
     //Remove the edge from node1 to node2, and also from node2 to node1.
     // If there are no such edges, then don't do anything.
     void removeEdge(int node1, int node2);
+
+	void DFStraversal(int node);
     
 private:
     std::vector<Node> adjList;
