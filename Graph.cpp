1ad54307ad94b70109388f80e23f93eca516af88
unknown <laufengd@EAS-BEN016D2.it.muohio.edu>
2014-12-06 15:11:36 -0500

    added throw for invalid addEdge param and a dfs traversal

--- Graph.cpp
+++ Graph.cpp
@@ -4,30 +4,56 @@
  * Sources  : All code is original
  */
 #include "Graph.h"
+#include <list>
+#include <stack>
 
 Graph::Graph(unsigned int numNodes){
 	adjList.resize(numNodes);
 }
 
 double Graph::getCost(int node1, int node2){
-	for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
-		if(adjList[node1].edgeList[i].dest == node2){
-			return adjList[node1].edgeList[i].cost;
-		}
-	} 
+	
+	int size = adjList.size(); //if a negative number is entered the function will return -1.0;
+	if(node1 < size){
+		for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
+			if(adjList[node1].edgeList[i].dest == node2){
+				return adjList[node1].edgeList[i].cost;
+			}
+		}	 
+	}
 	return -1.0;
 }
 
 //Add an edge from node1 to node2, and from node2 to node1, with
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
-	if(cost < 0) throw std::string("the cost is not valid");
+	
+	if(cost < 0){ //Throws string if given invalid cost
+		throw std::string("the cost is not valid");
+	}
+	
 
+	int size = adjList.size();
+
+	//program crashes if an invalid node is entered, so I created a throw::string exception
+	if(node1 < 0 || node1 >= size){ // throws string if given invalid node
+		throw std::string("node1 is not valid");
+	}
+	if(node2 < 0 || node2 >= size){
+		throw std::string("node2 is not valid");
+	}
+	
 
 	//if edge already exists, update cost
 	for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i ++){
 		if(adjList[node1].edgeList[i].dest == node2){
 			adjList[node1].edgeList[i].cost = cost;
+		}
+	}
+	for(unsigned int i = 0; i < adjList[node2].edgeList.size(); i++){
+		if(adjList[node2].edgeList[i].dest == node1){
+			adjList[node2].edgeList[i].cost = cost;
+			return; // exits function so that the pushback function doesn't occur if the edge already exists
 		}
 	}
 	
@@ -39,9 +65,11 @@ void Graph::addEdge(int node1, int node2, double cost){
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
-	for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){
-		
-		
+
+	//no need for 
+	
+	
+	for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i++){		
 		if(adjList[node1].edgeList[i].dest == node2){
 			//erase edge for node2 in node1's edgelist
 			adjList[node1].edgeList.erase(adjList[node1].edgeList.begin() + i);
@@ -54,4 +82,86 @@ void Graph::removeEdge(int node1, int node2){
 			}	
 		}
 	}
+}
+
+
+
+
+bool Graph::dfs(int node1, int node2) {
+ 
+	std::list<int> openList;
+	bool *closedList = new bool[adjList.size()];
+
+	openList.push_back(node1);
+
+	while(openList.size() > 0){
+		for (unsigned int i = 0; i < adjList[openList.front()].edgeList.size(); i++){
+			
+			if(adjList[openList.front()].edgeList[i].dest == node2){
+				return true;
+			}
+			
+			else if(closedList[adjList[openList.front()].edgeList[i].dest] == false){
+				openList.push_back((adjList[openList.front()].edgeList[i].dest));
+			}
+			
+		}
+		int index = openList.front();
+		closedList[index] = true;
+		openList.pop_front();
+	}
+	
+	delete[] closedList;
+
+	return false;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*bool *seen = new bool[adjList.size()];
+  
+	std::list<Node> q;
+  
+	q.push_back(adjList[node1]);
+  
+	seen[node1] = true;
+  
+	while (q.size() > 0) {
+		
+		 std::stack<int> edges;
+		 
+		 for (int i = 0; i < adjList[node1].edgeList.size(); i++) {
+			 int index = adjList[node1].edgeList[i].dest;
+
+			
+			 
+			 if (!seen[index]) {
+				q.push_back(j);
+				seen[j] = true;
+			}
+
+			if(index == node2) return true;
+			else{
+				return 
+			}
+		
+		
+			
+			
+			
+			int i = q.pop_front()
+      }
+    }
+  }
+  delete[] seen;
+  */
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 void testGraph(){
   Graph g(10);
   g.addEdge(0,1,50);
+  g.addEdge(0,4,100);
+  g.addEdge(0,2,75);
+  g.addEdge(2,1,60);
+
+
+
   if(g.getCost(1,0) != 50 || g.getCost(0,1) != 50){
     std::cout << "ERROR: Expected edge cost of 50, got " << g.getCost(1,0) << std::endl;
     exit(1);
@@ -11,11 +17,78 @@ void testGraph(){
     std::cout << "SUCCESS: Got the right edge cost" << std::endl;
   }
 
+   if(g.getCost(1,2) != 60 || g.getCost(2,1) != 60){
+    std::cout << "ERROR: Expected edge cost of 60, got " << g.getCost(1,0) << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Got the right edge cost" << std::endl;
+  }
+
+   if(g.getCost(0,4) != 100 || g.getCost(4,0) != 100){
+    std::cout << "ERROR: Expected edge cost of 100, got " << g.getCost(1,0) << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Got the right edge cost" << std::endl;
+  }
+
+   g.removeEdge(4,0);
+   if(g.getCost(0,4) != -1){
+    std::cout << "ERROR: Expected edge cost of -1, got " << g.getCost(2,3) << std::endl;
+    exit(1);
+   } else {
+    std::cout << "SUCCESS: Got the right edge cost for removed edge" << std::endl;
+   }
+
+
   if(g.getCost(2,3) != -1){
     std::cout << "ERROR: Expected edge cost of -1, got " << g.getCost(2,3) << std::endl;
     exit(1);
   } else {
     std::cout << "SUCCESS: Got the right edge cost for non-existent edge" << std::endl;
+  }
+
+  try {
+     g.addEdge(10,3, 3);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << std::endl;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+
+  }
+
+  try {
+     g.addEdge(-7, 3, 3);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception " << std::endl;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+
+  }
+   try {
+     g.addEdge(11, 3, -3);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << std::endl;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+  }
+
+  try {
+     g.addEdge(11, 3, -3);
+  } catch (std::string s) {
+    std::cout << "SUCCESS: Caught exception: " << std::endl;
+  } catch (...) {
+    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+  }
+
+  
+  g.addEdge(1,3, 12);
+  g.addEdge(1,3, 3);
+
+  if(g.getCost(1,3) != 3){
+    std::cout << "ERROR: Expected edge cost of 3, got " << g.getCost(1,3) << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Got the right edge cost updated edge" << std::endl;
   }
   
   g.removeEdge(1,0);
@@ -25,6 +98,9 @@ void testGraph(){
   } else {
     std::cout << "SUCCESS: Got the right edge cost for removed edge" << std::endl;
   }
+
+  std::cout << g.dfs(0, 4) << std::endl;
+
 
 }
 
