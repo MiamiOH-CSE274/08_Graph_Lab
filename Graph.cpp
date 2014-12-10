ef68a06b329c955a0de914064f48a6d0ed34d826
XCCOOP <country043@frontier.com>
2014-12-10 16:50:09 -0500

    Fixed add edge updating cost if already present

--- Graph.cpp
+++ Graph.cpp
@@ -32,13 +32,25 @@ double Graph::getCost(int node1, int node2){
 // the given cost. If the cost is < 0, throw a string exception.
 void Graph::addEdge(int node1, int node2, double cost){
 
-	if (cost < 0) {
+	if(cost < 0) {
 		throw std::string("Error: Cost can not be less than zero");
 	}
-
+	
+	if(node1 < adjList.size() && node2 < adjList.size()) {
+		for(unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
+			if(adjList[node1].edgeList[i].dest == node2) {
+				for(unsigned int j = 0; j < adjList[node1].edgeList.size(); j++) {
+					if(adjList[node2].edgeList[j].dest == node1) {
+						adjList[node2].edgeList[j].cost = cost;
+						adjList[node1].edgeList[i].cost = cost;
+						throw std::string("There is already an edge between these nodes, updating cost"); 
+					}
+				}
+			}
+		}
+	}
 	adjList[node1].edgeList.push_back(Edge(cost, node2));
 	adjList[node2].edgeList.push_back(Edge(cost, node1));
-
 }
 
 //Remove the edge from node1 to node2, and also from node2 to node1.
