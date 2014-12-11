b7187cda04577aac4a9db70418eaf0740a3f7bdb
GrasysER <grasyser@miamioh.edu>
2014-12-10 22:39:35 -0500

    DFT Method added along with checks if nodes are to big/small per feedback

--- Graph.cpp
+++ Graph.cpp
@@ -4,8 +4,11 @@
  * Sources  : All code is original
  */
 #include "Graph.h"
+#include <stack>
 
-// Basic for loop from Open Data Structures, and the rest from me
+// DFT pseudocode from StackOverFlow
+// Fixes for getCost and addEdge from Dr. Brinkmans feedback.
+// Basic for loop from Open Data Structures, and the rest from me.
 
 Graph::Graph(unsigned int numNodes){
 	adjList.resize(numNodes);
@@ -27,6 +30,12 @@ double Graph::getCost(int node1, int node2){
 void Graph::addEdge(int node1, int node2, double cost){
 	if (cost < 0){
 		throw std::string("Cost can't be negative.");
+	}
+	else if (node1 < 0 || node1 > adjList.size()){
+		throw std::string("Nodes cannot be negative size or bigger than the adjList");
+	}
+	else if (node2 < 0 || node2 > adjList.size()){
+		throw std::string("Nodes cannot be negative size or bigger than the adjList");
 	}
 
 	//edge exists , update cost
@@ -50,6 +59,13 @@ void Graph::addEdge(int node1, int node2, double cost){
 //Remove the edge from node1 to node2, and also from node2 to node1.
 // If there are no such edges, then don't do anything.
 void Graph::removeEdge(int node1, int node2){
+	if (node1 < 0 || node1 > adjList.size()){
+		throw std::string("Nodes cannot be negative size or bigger than the adjList");
+	}
+	else if (node2 < 0 || node2 > adjList.size()){
+		throw std::string("Nodes cannot be negative size or bigger than the adjList");
+	}
+
 	// remove specified edge from both nodes
 	for (unsigned int i = 0; i < adjList[node1].edgeList.size(); i++) {
 		if (adjList[node1].edgeList[i].dest == node2) {
@@ -64,3 +80,35 @@ void Graph::removeEdge(int node1, int node2){
 		}
 	}
 }
+
+//1 = untouched
+//2 = in OL
+//3 = in CL, no longer needed
+void Graph::DFT(int node){
+	if (node < 0 || node > adjList.size()){
+		throw std::string("Can't start with the specified node");
+	}
+
+	std::stack<int> openList;
+	std::vector<int> closedList;
+
+	closedList.resize(adjList.size());
+	openList.push(node);
+	closedList[node] = 2;
+
+	while (!openList.empty()){
+
+		int start = openList.top();
+		openList.pop();
+		closedList[start] = 3;
+
+		for (unsigned int i = 0; i < adjList[start].edgeList.size(); i++){
+			if (closedList[adjList[start].edgeList[i].dest] == 1){
+				openList.push(adjList[start].edgeList[i].dest);
+				closedList[adjList[start].edgeList[i].dest] = 2;
+			}
+		}
+	}
+}
+
+
